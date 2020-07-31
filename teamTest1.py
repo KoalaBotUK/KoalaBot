504797b70fedd53bb96c1876bf303ef7ce383d98
KieranA <ka5g18@soton.ac.uk>
2020-07-31 01:53:23 +0100

    Started work on re-doing team making to make code nicer

--- teamTest1.py
+++ teamTest1.py
@@ -20,7 +20,7 @@ matrix_values = [("a", 10, ["b", "c"], [], 1, 0, False), ("b", 9, ["c", "f"], []
                  ("f", 5, ["e", "a", "g"], [], 1, 0, False),
                  ("g", 4, ["e", "f"], [], 1, 0, False), ("h", 3, ["b", "c", "d"], [], 0, 0, False),
                  ("i", 2, ["h", "j"], [], 0, 0, False),
-                 ("j", 1, ["i"], [], 0, 0, False), ("k", 1, ["i"], [], 0, 0, True), ("l", 1, ["i"], [], 0, 0, True),
+                 ("j", 1, ["i"], [], 0, 0, False), ("k", 7, ["i"], [], 0, 0, True), ("l", 8, ["i"], [], 0, 0, True),
                  ("m", 1, ["i"], [], 0, 0, True)]
 
 subs_list = [("k", 7, ["i"], [], 0, 0, True), ("l", 8, ["i"], [], 0, 0, True), ("m", 1, ["i"], [], 3, 0, True)]
@@ -33,6 +33,10 @@ test_teams = [(['Team1', ('a', 10, ['b', 'c'], [], 1, 0, False), ('b', 9, ['c', 
               (['Team2', ('d', 7, ['h', 'i', 'j'], [], 0, 0, False), ('h', 3, ['b', 'c', 'd'], [], 0, 0, False),
                 ('j', 1, ['i'], [], 0, 0, False), ('f', 5, ['e', 'a', 'g'], [], 1, 0, False),
                 ('i', 2, ['h', 'j'], [], 0, 0, False)], 3.6, [])]
+
+
+def pick_teams(player_list):
+    return pick_teams_compa_list(player_list, [], player_list)
 
 
 # Compatibility value based purely on ranks.
@@ -62,7 +66,7 @@ def create_value_attitude(first_player, second_player):
     rank_difference = 1 - (calculate_rank_difference(first_player[1], second_player[1]) / rank_depreciation)
     if rank_difference == 0:
         return 0
-    if second_player[6]:
+    if second_player[6] | first_player[6]:
         return -1
     return (4 * want_to_play(first_player, second_player)) + (3 * check_attitude(first_player, second_player)) + (
             2 * rank_difference)
@@ -123,24 +127,8 @@ def highest_player_position(player_list, player):
     return player_list.index(player)
 
 
-def pick_teams_rank_only(player_list):
-    team = []
-    matrix = create_matrix(player_list)
-    highest_player = get_highest_player(player_list)
-    team.append(highest_player[0])
-    while len(team) < team_size:
-        highest_player_compa_list = matrix[highest_player_position(player_list, highest_player)]
-        next_player = get_next_player(highest_player_compa_list, player_list)
-        team.append(next_player[0])
-        player_list = remove_player(player_list, next_player)
-    player_list.remove(highest_player)
-    print(team)
-    if len(player_list) >= team_size:
-        pick_teams_rank_only(player_list)
-
-
 # TODO sort this fuck ugly method out
-def pick_teams_compa_list(player_list, input_teams):
+def pick_teams_compa_list(player_list, input_teams, untouched_player_list):
     team = []
     teams = input_teams
     team.append("Team" + str(len(teams) + 1))
@@ -149,14 +137,14 @@ def pick_teams_compa_list(player_list, input_teams):
     while len(team) < (team_size + 1):
         matrix = create_matrix(player_list)
         highest_player_compat_list = matrix[highest_player_position(player_list, highest_player)]
-        next_player = get_next_player(highest_player_compat_list, player_list)
+        next_player = get_next_player(highest_player_compat_list, remove_subs(player_list, get_sub_list(player_list)))
         team.append(next_player)
         player_list = remove_player(player_list, next_player)
     player_list.remove(highest_player)
     teams.append(team)
     if len(player_list) >= team_size:
-        pick_teams_compa_list(player_list, teams)
-    return teams
+        pick_teams_compa_list(player_list, teams, untouched_player_list)
+    return assign_subs(convert_teams(teams), get_sub_list(untouched_player_list))
 
 
 def get_highest(compat_list):
@@ -249,7 +237,9 @@ def team_rank_average(team):
 
 
 def assign_subs(teams, sub_list):
+    print(teams)
     av_ranks = team_av_rank_list(teams)
+    print(av_ranks)
     for x in sub_list:
         teams[get_team_from_rank(take_closest(x[1], av_ranks), teams)][2].append(x)
     return teams
@@ -272,7 +262,15 @@ def get_team_from_rank(rank, teams):
             return teams.index(x)
 
 
+def reorganise_team_list(teams):
+    teams_list = []
+    for x in teams:
+        teams_list.append((x[0], x[2]))
+
+
 # print(get_team_from_rank(take_closest(7, team_av_rank_list(test_teams)), test_teams))
 # print(assign_subs(test_teams, subs_list))
+# print(pick_teams_compa_list(matrix_values, [], matrix_values))
+# print_matrix(create_matrix(matrix_values))
+# print(pick_teams(matrix_values))
 
-print(pick_teams_compa_list(matrix_values, []))
