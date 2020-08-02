64bb393b2686fe6d400058b54b8e0fc847c0d97e
Robert Sławik <robertslawik@outlook.com>
2020-08-02 16:47:00 +0100

    Add Facebook page and post wrapper functions

--- cogs/SocialAlert.py
+++ cogs/SocialAlert.py
@@ -30,20 +30,49 @@ import KoalaBot
 # Constants
 load_dotenv()
 
-
 # Variables
 
-token = {}
 
-graph = facebook.GraphAPI(token)
 
 
 class SocialAlert(commands.Cog):
-    '''
+    """
         A discord.py cog for providing social feed updates from Facebook, Instagram and Twitter
-    '''
-    pass
+    """
 
+
+pass
+
+
+class FacebookGraphAPIHandler:
+    """
+    A wrapper to interact with the Facebook GraphAPI
+    """
+    def __init__(self, client_id: str, client_secret: str):
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.oauth_token = self.get_new_facebook_oauth()
+        self.graph = facebook.GraphAPI(self.oauth_token)
+
+
+    def get_new_facebook_oauth(self):
+        pass
+
+    def get_post_info(self, page_id):
+        """
+        Gets default fields for the most recent post on a page feed
+        :param page_id: ID of the page
+        :return: Dictionary with the post's data
+        """
+        return self.graph.get_connections(page_id, connection_name="feed", limit=1)
+
+    def get_page_info(self, page_id):
+        """
+        Gets default fields (name and id) for a specified page
+        :param page_id: ID of the page
+        :return: Dictionary with the page's data
+        """
+        return self.graph.get_object(page_id)
 
 def setup(bot: KoalaBot) -> None:
     """
