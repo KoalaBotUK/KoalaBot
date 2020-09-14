75cf92f42e9283c74cd671d11d3fcb096e1d45a9
Stefan Cooper <stefan.cooper27@gmail.com>
2020-09-14 08:29:34 +0100

    Refactored tests and added list filtered words test

--- tests/test_TextFilter.py
+++ tests/test_TextFilter.py
@@ -21,6 +21,7 @@ from cogs import TextFilter
 from tests.utils import TestUtilsCog
 from tests.utils.TestUtils import assert_activity
 from utils import KoalaDBManager
+from utils.KoalaColours import *
 
 # Constants
 
@@ -45,37 +46,38 @@ def setup_function():
     print("Tests starting")
 
 @pytest.mark.asyncio()
-async def test_filter_new_word_correct_text():
-    await dpytest.message(KoalaBot.COMMAND_PREFIX + "filter_word no")
-    dpytest.verify_message("*no* has been filtered.")
-    dpytest.verify_message("Watch your language! Your message: '*k!filter_word no*' in "+dpytest.get_config().guilds[0].channels[0].mention +" has been deleted by KoalaBot.")
-    
-@pytest.mark.asyncio()
-async def test_unfilter_word_correct_text():
-    await dpytest.message(KoalaBot.COMMAND_PREFIX + "filter_word unfilterboi")
-    dpytest.verify_message("*unfilterboi* has been filtered.")
-    dpytest.verify_message("Watch your language! Your message: '*k!filter_word unfilterboi*' in "+dpytest.get_config().guilds[0].channels[0].mention +" has been deleted by KoalaBot.")
-    #dpytest.verify_message(dpytest.get_config().guilds[0].channels[0].mention)
-    await dpytest.message(KoalaBot.COMMAND_PREFIX + "unfilter_word unfilterboi")
-    dpytest.verify_message("*unfilterboi* has been unfiltered.")
-
-@pytest.mark.asyncio()
 async def test_filter_new_word_correct_database():
     old = len(tf_cog.tf_database_manager.database_manager.db_execute_select("SELECT filtered_text FROM TextFilter WHERE filtered_text = 'no'"))
     await dpytest.message(KoalaBot.COMMAND_PREFIX + "filter_word no")
+    dpytest.verify_message("*no* has been filtered.")
+    dpytest.verify_message("Watch your language! Your message: '*k!filter_word no*' in "+dpytest.get_config().guilds[0].channels[0].mention +" has been deleted by KoalaBot.")
     assert len(tf_cog.tf_database_manager.database_manager.db_execute_select("SELECT filtered_text FROM TextFilter WHERE filtered_text = 'no'")) == old + 1 
 
 @pytest.mark.asyncio()
 async def test_unfilter_word_correct_database():
     await dpytest.message(KoalaBot.COMMAND_PREFIX + "filter_word unfilterboi")
+    dpytest.verify_message("*unfilterboi* has been filtered.")
+    dpytest.verify_message("Watch your language! Your message: '*k!filter_word unfilterboi*' in "+dpytest.get_config().guilds[0].channels[0].mention +" has been deleted by KoalaBot.")
+    
     old = len(tf_cog.tf_database_manager.database_manager.db_execute_select("SELECT filtered_text FROM TextFilter WHERE filtered_text = 'unfilterboi'"))
     await dpytest.message(KoalaBot.COMMAND_PREFIX + "unfilter_word unfilterboi")
     assert len(tf_cog.tf_database_manager.database_manager.db_execute_select("SELECT filtered_text FROM TextFilter WHERE filtered_text = 'unfilterboi';")) == old - 1  
+    dpytest.verify_message("*unfilterboi* has been unfiltered.")
 
-# @pytest.mark.asyncio()
-# async def test_list_filtered_words():
-#     await dpytest.message(KoalaBot.COMMAND_PREFIX + "filter_word filterboi")
-#     old = len(tf_cog.tf_database_manager.database_manager.db_execute_select("SELECT filtered_text FROM TextFilter WHERE filtered_text = 'unfilterboi'"))
-#     await dpytest.message(KoalaBot.COMMAND_PREFIX + "unfilter_word unfilterboi")
-#     assert len(tf_cog.tf_database_manager.database_manager.db_execute_select("SELECT filtered_text FROM TextFilter WHERE filtered_text = 'unfilterboi';")) == old - 1  
+@pytest.mark.asyncio()
+async def test_list_filtered_words():
+    await dpytest.message(KoalaBot.COMMAND_PREFIX + "filter_word listing1")
+    dpytest.verify_message("*listing1* has been filtered.")
+    dpytest.verify_message("Watch your language! Your message: '*k!filter_word listing1*' in "+dpytest.get_config().guilds[0].channels[0].mention +" has been deleted by KoalaBot.")
+    await dpytest.message(KoalaBot.COMMAND_PREFIX + "filter_word listing2")
+    dpytest.verify_message("*listing2* has been filtered.")
+    dpytest.verify_message("Watch your language! Your message: '*k!filter_word listing2*' in "+dpytest.get_config().guilds[0].channels[0].mention +" has been deleted by KoalaBot.")
+
+    assert_embed = discord.Embed()
+    assert_embed.title = "Filtered Words"
+    assert_embed.colour = KOALA_GREEN
+    assert_embed.set_footer(text=f"Guild ID: {dpytest.get_config().guilds[0].id}")
+    assert_embed.add_field(name="Banned Words", value="listing1\nlisting2\n")
+    await dpytest.message(KoalaBot.COMMAND_PREFIX + "check_filtered_words")
+    dpytest.verify_embed(embed=assert_embed)
 
