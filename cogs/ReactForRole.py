0b122aa489c85d486ccce36f0cda7735fe3727e6
Anan Venkatesh <av1u19@soton.ac.uk>
2020-10-25 23:25:21 +0000

    Update cogs/ReactForRole.py
    Co-authored-by: Jack Draper <drapj002@gmail.com>

--- cogs/ReactForRole.py
+++ cogs/ReactForRole.py
@@ -35,7 +35,7 @@ class ReactForRole(commands.Cog):
         self.rfr_database_manager = ReactForRoleDBManager(KoalaBot.database_manager)
         self.rfr_database_manager.create_tables()
 
-    @commands.group(name="reactForRole", aliases=["rfr", "react_for_role", "ReactForRole"])
+    @commands.group(name="rfr", aliases=["reactForRole", "react_for_role"])
     async def react_for_role_group(self, ctx: commands.Context):
         return
 
