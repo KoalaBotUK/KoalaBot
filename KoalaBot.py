1afd1ad575d2ba82d7360163d2d0bc5781b7cf3f
Jack Draper <drapj002@gmail.com>
2020-12-14 23:42:13 +0000

    Verifyfix (#67)
    * updated to use fetch_member instead of get_member when it can't find the user in the local cache
    
    * Added intents
    
    Co-authored-by: harry <harry@yark.org.uk>
    
    Pushed ahead of passing travix

--- KoalaBot.py
+++ KoalaBot.py
@@ -50,7 +50,9 @@ PERMISSION_ERROR_TEXT = "This guild does not have this extension enabled, go to 
 KOALA_IMAGE_URL = "https://cdn.discordapp.com/attachments/737280260541907015/752024535985029240/discord1.png"
 # Variables
 started = False
-client = commands.Bot(command_prefix=COMMAND_PREFIX)
+
+intents = discord.Intents(messages=True, members=True)
+client = commands.Bot(command_prefix=COMMAND_PREFIX, intents=intents)
 database_manager = DBManager(DATABASE_PATH, DB_KEY)
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)-8s %(message)s')
 logger = logging.getLogger('discord')
