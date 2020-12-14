1413378a2196a5f8b7feec343d31738f6bb98466
t1ngson <74460709+t1ngson@users.noreply.github.com>
2020-12-14 18:49:52 +0000

    Added comments to Insights

--- cogs/Insights.py
+++ cogs/Insights.py
@@ -5,14 +5,12 @@ Koala Bot Insights Code
 Created by: Samuel Tiongson
 """
 from discord.ext import commands
-
 import KoalaBot
 
 
 class Insights(commands.Cog):
     """
     A discord.py cog pertaining to showing the insights of the KoalaBot
-
     """
 
     def __init__(self, bot, database_manager=None):
@@ -44,30 +42,46 @@ class Insights(commands.Cog):
         :param ctx: The discord context
         :param arg: Searches for guilds with argument provided
         """
+        # Retrieves AsyncIterator
         guild_list = self.bot.fetch_guilds()
         guild_list_names = []
 
+        # Cycle through iterator, check if there is an arg, if there is check against the arg, if not just append the
+        # guild name
         async for guild in guild_list:
             if arg is not None:
+                # Change arg and guild name to uppercase, and split guild name by spaces
                 if arg.upper() in guild.name.upper().split(" "):
                     guild_list_names.append(guild.name)
             else:
                 guild_list_names.append(guild.name)
 
+        # If there are no guilds and no arguments
         if len(guild_list_names) == 0 and arg is None:
             await ctx.send("KoalaBot is in no servers!")
+
+        # If there are no guilds but there are arguments
         elif len(guild_list_names) == 0 and arg is not None:
             await ctx.send(f"No servers with {arg} in their name!")
+
+        # There must be guilds in the list
         else:
             string_to_send = ''
+            # While there are guilds in the list, run code
             while len(guild_list_names) != 0:
+                # Get the length of the first server name
                 length = len(guild_list_names[0])
+                # If this length + the current string length + 2 for comma and space is greater than 2000
                 if len(string_to_send) + length + 2 > 2000:
+                    # Print the string and reset it to nothing
                     await ctx.send(string_to_send)
                     string_to_send = ''
                 else:
+                    # If the above is not true, then pop the server name from the list and add it to the string
                     guild = guild_list_names.pop(0)
                     string_to_send += guild + ", "
+
+            # Remove the comma and space at the end of the string
             await ctx.send(string_to_send[:-2])
 
 
