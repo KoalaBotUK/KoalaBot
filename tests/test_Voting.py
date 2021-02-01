69e533e8c9e01d954617c81352cf0283ca14d7c1
harry <harry@yark.org.uk>
2021-02-01 17:43:28 +0000

    moved everything back into one file

--- tests/test_Voting.py
+++ tests/test_Voting.py
@@ -20,7 +20,7 @@ from discord.ext import commands
 # Own modules
 import KoalaBot
 from cogs import Voting
-from utils.KoalaDBManager import KoalaDBManager
+
 
 class Fake:
     def __getattr__(self, item):
@@ -77,3 +77,62 @@ async def test_vote():
     # await dpytest.message(f"{KoalaBot.COMMAND_PREFIX}vote send")
     # dpytest.verify_message(f"Sent vote to 1 users")
     # errors trying to send embed because I think an old version of discord.py is wanting it as a dict? not sure
+
+
+def test_two_way():
+    def test_asserts(f, *args, **kwargs):
+        try:
+            f(*args, **kwargs)
+        except AssertionError:
+            return
+        raise AssertionError
+
+    # test internal asserts don't false positive
+    t = Voting.TwoWay({1: 2, 3: 4})
+    t2 = Voting.TwoWay({1: 2, 2: 1, 4: 3})
+    assert t == t2
+
+    # test an invalid dict cannot be made
+    test_asserts(Voting.TwoWay, {1: 2, 2: 3})
+
+    def ta2():
+        t = Voting.TwoWay()
+        t[1] = 2
+        t[2] = 3
+        test_asserts(ta2)
+
+
+def test_vote_manager_general():
+    vm = Voting.VoteManager()
+    assert not vm.active_votes
+    vote = vm.create_vote(ctx, "Test Vote")
+    assert ctx.author.id in vm.active_votes.keys()
+    assert vm.get_vote(ctx) == vote
+    assert vm.has_active_vote(1234)
+    vm.cancel_vote(1234)
+    assert not vm.has_active_vote(1234)
+
+
+def test_vote_general():
+    vm = Voting.VoteManager()
+    vote = vm.create_vote(ctx, "Test Vote")
+    assert vote.id == ctx.author.id and vote.guild == ctx.guild.id and vote.title == "Test Vote"
+    assert not vote.is_ready()
+
+    vote.add_role(7890)
+    assert 7890 in vote.target_roles
+    vote.remove_role(7890)
+    assert 7890 not in vote.target_roles
+
+    vote.set_vc(1234)
+    assert vote.target_voice_channel == 1234
+
+    opt1 = Voting.Option("test option 1", "test body 1")
+    opt2 = Voting.Option("test option 2", "test body 2")
+    vote.add_option(opt1)
+    vote.add_option(opt2)
+    assert len(vote.options) == 2
+    assert vote.is_ready()
+
+    vote.remove_option(1)
+    assert vote.options[0] == opt2
