d6ba7bce7c915cefd17a747305cb4171f96b06ea
Anan Venkatesh <av1u19@soton.ac.uk>
2021-02-08 10:09:53 +0000

    Add debug command for python evaluation in disc

--- cogs/BaseCog.py
+++ cogs/BaseCog.py
@@ -7,17 +7,18 @@ Commented using reStructuredText (reST)
 """
 # Futures
 
+import inspect
 # Built-in/Generic Imports
+import io
+import textwrap
+import traceback
+from contextlib import redirect_stdout
 
 # Libs
-import inspect
-
-import discord
 from discord.ext import commands
 
 # Own modules
 import KoalaBot
-from utils.KoalaUtils import extract_id, error_embed
 from utils.KoalaColours import *
 
 
@@ -220,7 +221,89 @@ class BaseCog(commands.Cog, name='KoalaBot'):
 
         await ctx.send(embed=embed)
 
+    @commands.command(name="debug", hidden=True)
+    @commands.check(KoalaBot.is_owner)
+    async def debug(self, ctx, *, body: str):
+        """Evaluates code."""
 
+        blocked_words = ['.delete()', 'os', 'subprocess', 'history()', '("token")', "('token')"]
+        for x in blocked_words:
+            if x in body:
+                return await ctx.send('Your code contains certain blocked words.')
+        env = {
+            'ctx': ctx,
+            'channel': ctx.channel,
+            'author': ctx.author,
+            'guild': ctx.guild,
+            'message': ctx.message,
+            'source': inspect.getsource,
+        }
+
+        env.update(globals())
+
+        env.update(locals())
+
+        body = self.cleanup_code(body)
+        stdout = io.StringIO()
+        err = out = None
+
+        to_compile = f'async def func():\n{textwrap.indent(body, "  ")}'
+
+        def paginate(text: str):
+            '''Simple generator that paginates text.'''
+            last = 0
+            pages = []
+            for curr in range(0, len(text)):
+                if curr % 1980 == 0:
+                    pages.append(text[last:curr])
+                    last = curr
+                    appd_index = curr
+            if appd_index != len(text) - 1:
+                pages.append(text[last:curr])
+            return list(filter(lambda a: a != '', pages))
+
+        try:
+            exec(to_compile, env)
+        except Exception as e:
+            err = await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
+            return await ctx.message.add_reaction('\u2049')
+
+        func = env['func']
+        try:
+            with redirect_stdout(stdout):
+                ret = await func()
+        except Exception as e:
+            value = stdout.getvalue()
+            err = await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
+        else:
+            value = stdout.getvalue()
+            if ret is None:
+                if value:
+                    await ctx.send(format(value))
+            else:
+                self.bot._last_result = ret
+                out = await ctx.send(f'```py\n{value}{ret}\n```')
+
+        if out:
+            await ctx.message.add_reaction('\u2705')  # tick
+        elif err:
+            await ctx.message.add_reaction('\u2049')  # x
+        else:
+            await ctx.message.add_reaction('\u2708')
+
+    def cleanup_code(self, content):
+        """Automatically removes code blocks from the code."""
+        # remove ```py\n```
+        if content.startswith('```') and content.endswith('```'):
+            return '\n'.join(content.split('\n')[1:-1])
+
+        # remove `foo`
+        return content.strip('` \n')
+
+    def get_syntax_error(self, e):
+        if e.text is None:
+            return f'```py\n{e.__class__.__name__}: {e}\n```'
+        return f'```py\n{e.text}{"^":>{e.offset}}\n{e.__class__.__name__}: {e}```'
 
 
 def setup(bot: KoalaBot) -> None:
