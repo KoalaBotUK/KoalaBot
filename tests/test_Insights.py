21c2d9c12abcd9972f3e4211903331c1627449e4
Anan Venkatesh <av1u19@soton.ac.uk>
2021-06-28 12:43:54 +0100

    Implement k!insights test

--- tests/test_Insights.py
+++ tests/test_Insights.py
@@ -8,55 +8,74 @@ Commented using reStructuredText (reST)
 # Futures
 
 # Built-in/Generic Imports
+
 import random
-from typing import *
 
 # Libs
 import discord.ext.test as dpytest
 import pytest
-from discord.ext import commands
 
 # Own modules
 import KoalaBot
-from cogs import Insights
-from tests.utils import TestUtilsCog
+from cogs import Insights, BaseCog
+from tests.utils_testing import LastCtxCog
 
 # Constants
 
 # Variables
-insights_cog: Insights.Insights = None
-utils_cog: TestUtilsCog.TestUtilsCog = None
 
-def setup_function():
-    """ setup any state specific to the execution of the given module."""
-    global insights_cog
-    global utils_cog
-    bot: commands.Bot = commands.Bot(command_prefix=KoalaBot.COMMAND_PREFIX)
-    insights_cog = Insights.Insights(bot)
-    utils_cog = TestUtilsCog.TestUtilsCog(bot)
-    bot.add_cog(insights_cog)
+@pytest.fixture(scope="function", autouse=True)
+def utils_cog(bot):
+    utils_cog = LastCtxCog.LastCtxCog(bot)
     bot.add_cog(utils_cog)
     dpytest.configure(bot)
     print("Tests starting")
+    return utils_cog
+
+
+@pytest.fixture(scope="function", autouse=True)
+def base_cog(bot):
+    base_cog = BaseCog.BaseCog(bot)
+    bot.add_cog(base_cog)
+    dpytest.configure(bot)
+    print("Tests starting")
+    return base_cog
+
+
+@pytest.fixture(scope="function", autouse=True)
+async def insights_cog(bot):
+    insights_cog = Insights.Insights(bot)
+    bot.add_cog(insights_cog)
+    dpytest.configure(bot)
+    print("Tests starting")
+    return insights_cog
+
 
 @pytest.mark.asyncio
-async def test_insights():
+@pytest.mark.parametrize("num_guilds, num_members",
+                         [(1, 1), (1, 2), (1, 10), (2, 2), (2, 5), (2, 20), (5, 100), (100, 10000), (20, 20000)])
+async def test_insights(num_guilds, num_members):
     test_config = dpytest.get_config()
-    expected_users = 1
-    for i in range(10):
-        guild = dpytest.back.make_guild(f"Test Guild {i}")
-        test_config.guilds.append(guild)
-        expected_users += 1
-        await dpytest.message(KoalaBot.COMMAND_PREFIX + "insights")
-        dpytest.verify_message(
-            f"KoalaBot is in {len(dpytest.get_config().guilds)} servers with a member total of {expected_users}.")
-    import random
-    for i in range(100):
-        await dpytest.member_join(random.randint(0,len(test_config.guilds) - 1))
-        expected_users += 1
-        await dpytest.message(KoalaBot.COMMAND_PREFIX + "insights")
-        dpytest.verify_message(
-            f"KoalaBot is in {len(dpytest.get_config().guilds)} servers with a member total of {expected_users}.")
+
+    for i in range(num_guilds):
+        g = dpytest.back.make_guild(f"TestGuild {i}")
+        if test_config.guilds[i] is None:
+            test_config.guilds[i] = g
+        else:
+            test_config.guilds.append(g)
+
+    for i in range(1, num_members):
+        m = dpytest.back.make_user(f"TestUser {i}", random.randint(1, 9999))
+        dpytest.back.make_member(m, random.choice(test_config.guilds))
+
+    expected_users = 0
+    for g in test_config.guilds:
+        expected_users += g.member_count
+
+    await dpytest.message(KoalaBot.COMMAND_PREFIX + "insights")
+    assert dpytest.verify().message().content(
+        f"KoalaBot is in {len(dpytest.get_config().guilds)} servers with a member total of {expected_users}.")
+
 
 @pytest.mark.asyncio
 async def test_servers_no_args():
@@ -70,8 +89,8 @@ async def test_servers_no_args():
         guild = dpytest.back.make_guild(f"Test Guild {i}")
         expected += f"Test Guild {i}, "
         test_config.guilds.append(guild)
-        await dpytest.member_join(i,client.user)
-        await dpytest.message(KoalaBot.COMMAND_PREFIX+"servers",i)
+        await dpytest.member_join(i, client.user)
+        await dpytest.message(KoalaBot.COMMAND_PREFIX + "servers", i)
         await dpytest.verify_message(expected[:-2])
 
 
@@ -90,6 +109,7 @@ async def test_servers_fail_args():
         await dpytest.message(KoalaBot.COMMAND_PREFIX + f"servers {arg}", i)
         await dpytest.verify_message(f"No servers with {arg} in their name")
 
+
 @pytest.mark.asyncio
 async def test_servers_with_args():
     test_config = dpytest.get_config()
@@ -104,4 +124,3 @@ async def test_servers_with_args():
         await dpytest.member_join(i, client.user)
         await dpytest.message(KoalaBot.COMMAND_PREFIX + f"servers {arg}", i)
         await dpytest.verify_message("Test Guild 0")
-
