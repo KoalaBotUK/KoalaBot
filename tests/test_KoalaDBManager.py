ea0fceff4dd600d8a182307a3a71954cc0c1ad59
Jack Draper <drapj002@gmail.com>
2021-07-28 16:18:33 +0100

    remove excess imports

--- tests/test_KoalaDBManager.py
+++ tests/test_KoalaDBManager.py
@@ -22,54 +22,54 @@ from utils import KoalaDBManager
 # Variables
 
 
-@mock.patch("utils.KoalaDBManager.os_name", "nt")
+@mock.patch("os.name", "nt")
 def test_format_db_path_windows_absolute_full():
     db_path = KoalaDBManager.format_db_path("C:/test_dir/", "test.db")
     assert db_path == "C:/test_dir/windows_test.db"
 
 
-@mock.patch("utils.KoalaDBManager.os_name", "nt")
+@mock.patch("os.name", "nt")
 def test_format_db_path_windows_absolute_partial():
     db_path = KoalaDBManager.format_db_path("C:/test_dir", "test.db")
     assert db_path == "C:/test_dir/windows_test.db"
 
 
-@mock.patch("utils.KoalaDBManager.os_name", "nt")
+@mock.patch("os.name", "nt")
 @mock.patch("os.getcwd", mock.MagicMock(return_value="C:/"))
 def test_format_db_path_windows_relative_full():
     db_path = KoalaDBManager.format_db_path("/test_dir/", "test.db")
     assert db_path == "C:/test_dir/windows_test.db"
 
 
-@mock.patch("utils.KoalaDBManager.os_name", "nt")
+@mock.patch("os.name", "nt")
 @mock.patch("os.getcwd", mock.MagicMock(return_value="C:/"))
 def test_format_db_path_windows_relative_backslash():
     db_path = KoalaDBManager.format_db_path("\\test_dir\\", "test.db")
     assert db_path == "C:/test_dir/windows_test.db"
 
 
-@mock.patch("utils.KoalaDBManager.os_name", "nt")
+@mock.patch("os.name", "nt")
 @mock.patch("os.getcwd", mock.MagicMock(return_value="C:/"))
 def test_format_db_path_windows_relative_partial():
     db_path = KoalaDBManager.format_db_path("test_dir/", "test.db")
     assert db_path == "C:/test_dir/windows_test.db"
 
 
-@mock.patch("utils.KoalaDBManager.os_name", "posix")
+@mock.patch("os.name", "posix")
 @mock.patch("utils.KoalaDBManager.ENCRYPTED_DB", False)
 def test_format_db_path_linux_absolute_unencrypted():
     db_path = KoalaDBManager.format_db_path("/test_dir/", "test.db")
     assert db_path == "/test_dir/windows_test.db"
 
 
-@mock.patch("utils.KoalaDBManager.os_name", "posix")
+@mock.patch("os.name", "posix")
 @mock.patch("utils.KoalaDBManager.ENCRYPTED_DB", True)
 def test_format_db_path_linux_absolute_encrypted():
     db_path = KoalaDBManager.format_db_path("/test_dir/", "test.db")
     assert db_path == "/test_dir/test.db"
 
 
-@mock.patch("utils.KoalaDBManager.os_name", "posix")
+@mock.patch("os.name", "posix")
 @mock.patch("utils.KoalaDBManager.ENCRYPTED_DB", True)
 def test_format_db_path_linux_relative():
     db_path = KoalaDBManager.format_db_path("test_dir/", "test.db")
