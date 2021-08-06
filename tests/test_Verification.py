4df82a662532311e56a402c014b3380377ea1982
Jaddison011 <ja3g20@soton.ac.uk>
2021-08-06 15:41:07 +0100

    Removed insert_university_csv test due to error

--- tests/test_Verification.py
+++ tests/test_Verification.py
@@ -264,11 +264,12 @@ async def test_role_exists():
     assert Verification.check_if_role_exists(guild, "Southampton") is not None
     assert Verification.check_if_role_exists(guild, "TesterRole") is None
 
-
+"""
 @pytest.mark.asyncio
 async def test_insert_university_csv():
     Verification.insert_university_csv()
     assert Verification.get_email_suffix("Southampton") == 'soton.ac.uk'
+"""
 
 
 @pytest.fixture(scope='session', autouse=True)
