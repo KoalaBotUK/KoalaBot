3e6beb2a20e2c3167815a9103eb4ed0cd0f65ec9
Jack Draper <drapj002@gmail.com>
2021-10-14 21:49:04 +0100

    fix: add pysqlcipher driver to sqlalchemy

--- base_models.py
+++ base_models.py
@@ -3,10 +3,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from KoalaBot import DATABASE_PATH
+from utils.KoalaDBManager import ENCRYPTED_DB
+import os
+
+if os.name == 'nt' or not ENCRYPTED_DB:
+    protocol = "sqlite:///"
+else:
+    protocol = "sqlite+pysqlcipher:///"
 
 Base = declarative_base()
-ff = "sqlite:///"+DATABASE_PATH
-engine = create_engine(ff, future=True, echo=True)
+connection_url = protocol+DATABASE_PATH
+engine = create_engine(connection_url, future=True, echo=True)
 Session = sessionmaker(future=True)
 Session.configure(bind=engine)
 session = Session()
