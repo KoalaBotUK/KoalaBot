8dd0403bb8dd553eff3a293adf6d9383304a63a1
Kaspiaan <kjallinson@gmail.com>
2021-10-20 19:46:27 +0100

    Updated authors to be senior devs and credits to be anyone that has contributed

--- KoalaBot.py
+++ KoalaBot.py
@@ -6,16 +6,13 @@ Run this to start the Bot
 
 Commented using reStructuredText (reST)
 """
-__author__ = "Jack Draper, Kieran Allinson, Viraj Shah," \
-               "Anan Venkatesh, Harry Nelson, Robert Slawik, Rudra Malik, Stefan Cooper, Bill Cao," \
-               "Samuel Tiongson, Aqeel Little, Charlie Bowe, see full list of developers at: https://koalabot.uk/"
+__author__ = "Jack Draper, Kieran Allinson, Viraj Shah, Stefan Cooper, Otto Hooper"
 __copyright__ = "Copyright (c) 2020 KoalaBot"
-__credits__ = ["Jack Draper", "Kieran Allinson", "Viraj Shah",
-               "Anan Venkatesh", "Harry Nelson", "Robert Slawik", "Rudra Malik", "Stefan Cooper", "Bill Cao",
-               "Samuel Tiongson", "Aqeel Little", "Charlie Bowe", "see full list of developers at: https://koalabot.uk/"]
+__credits__ = ["Jack Draper", "Kieran Allinson", "Viraj Shah", "Stefan Cooper", "Anan Venkatesh", "Harry Nelson", "Bill Cao",
+               "Aqeel Little", "Charlie Bowe", "Oluwaponmile Femi-Sunmaila", "see full list of developers at: https://koalabot.uk/"]
 __license__ = "MIT License"
 __version__ = "0.4.5"
-__maintainer__ = "Jack Draper, Kieran Allinson, Viraj Shah, Stefan Cooper"
+__maintainer__ = "Jack Draper, Kieran Allinson, Viraj Shah, Stefan Cooper, Otto Hooper"
 __email__ = "koalabotuk@gmail.com"
 __status__ = "Development"  # "Prototype", "Development", or "Production"
 
