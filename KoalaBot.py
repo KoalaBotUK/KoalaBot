2fb73477753b92d3bf2bacadecb741b3edc0197d
Kaspiaan <kjallinson@gmail.com>
2021-10-21 01:33:27 +0100

    Updated Poms name at his request

--- KoalaBot.py
+++ KoalaBot.py
@@ -9,7 +9,7 @@ Commented using reStructuredText (reST)
 __author__ = "Jack Draper, Kieran Allinson, Viraj Shah, Stefan Cooper, Otto Hooper"
 __copyright__ = "Copyright (c) 2020 KoalaBot"
 __credits__ = ["Jack Draper", "Kieran Allinson", "Viraj Shah", "Stefan Cooper", "Anan Venkatesh", "Harry Nelson", "Bill Cao",
-               "Aqeel Little", "Charlie Bowe", "Oluwaponmile Femi-Sunmaila", "see full list of developers at: https://koalabot.uk/"]
+               "Aqeel Little", "Charlie Bowe", "Ponmile Femi-Sunmaila", "see full list of developers at: https://koalabot.uk/"]
 __license__ = "MIT License"
 __version__ = "0.4.5"
 __maintainer__ = "Jack Draper, Kieran Allinson, Viraj Shah, Stefan Cooper, Otto Hooper"
