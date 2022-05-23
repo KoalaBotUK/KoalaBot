b60fd8d3495464f801adb0db64d6fd05315b70ea
Otto Hooper <votton.hooper@gmail.com>
2022-05-23 14:37:44 +0100

    Move cog functionality to announce_message

--- koala/cogs/announce/cog.py
+++ koala/cogs/announce/cog.py
@@ -1,116 +1,95 @@
-# Built-in/Generic Imports
-import math
-import time
-
-# Libs
-import discord
-from discord.ext import commands
-
-# Own modules
+# Own imports
 import koalabot
-from koala.colours import KOALA_GREEN
-from koala.db import insert_extension
-from koala.utils import extract_id, wait_for_message
-from .announce_message import AnnounceMessage
-from .db import AnnounceDBManager
-from .log import logger
-from .utils import ANNOUNCE_SEPARATION_DAYS, SECONDS_IN_A_DAY, MAX_MESSAGE_LENGTH
+from announce_message import AnnounceMessage, Announce, announce_is_enabled
 
+def announce_is_enabled(guild):
+    return announce_is_enabled(guild)
 
-def announce_is_enabled(ctx):
+def not_exceeded_limit(self, guild_id, ctx):
     """
-    A command used to check if the guild has enabled announce
-    e.g. @commands.check(announce_is_enabled)
-
+    Check if the number of announcements in the guild is not exceeded
+    :param guild_id: The id of the guild
     :param ctx: The context of the message
-    :return: True if enabled or test, False otherwise
+    :return: True if the number of announcements is not exceeded, False otherwise
     """
     try:
-        result = koalabot.check_guild_has_ext(ctx, "Announce")
+        result = self.Announce.not_exceeded_limit(guild_id, ctx)
     except PermissionError:
         result = False
 
     ctx.send(result or (str(ctx.guild) == koalabot.TEST_USER and koalabot.is_dpytest))
 
-
-class Announce(commands.Cog):
+def has_active_msg(self, guild_id, ctx):
     """
-        Send DM announcements to certain roles and people.
+    Check if there is an active announcement message
+    :param guild_id: The id of the guild
+    :param ctx: The context of the message
+    :return: True if there is an active announcement message, False otherwise
     """
+    try:
+        result = self.Announce.has_active_msg(guild_id, ctx)
+    except PermissionError:
+        result = False
 
-    def __init__(self, bot):
-        self.bot = bot
-        self.messages = {}
-        self.roles = {}
-        insert_extension("Announce", 0, True, True)
-        self.announce_database_manager = AnnounceDBManager()
+    ctx.send(result or (str(ctx.guild) == koalabot.TEST_USER and koalabot.is_dpytest))
 
-    def not_exceeded_limit(self, guild_id, ctx):
-        """
-        Check if enough days have passed for the user to use the announce function
-        :return:
-        """
-        if self.announce_database_manager.get_last_use_date(guild_id):
-            return int(time.time()) - self.announce_database_manager.get_last_use_date(
-                guild_id) > ANNOUNCE_SEPARATION_DAYS * SECONDS_IN_A_DAY
-        ctx.send(True)
+def get_role_names(self, guild_id, roles, ctx):
+    """
+    Get the names of the roles
+    :param guild_id: The id of the guild
+    :param roles: The roles
+    :param ctx: The context of the message
+    :return: A string consisting the names of the roles
+    """
+    try:
+        result = self.Announce.get_role_names(guild_id, roles, ctx)
+    except PermissionError:
+        result = False
 
-    def has_active_msg(self, guild_id, ctx):
-        """
-        Check if a particular id has an active announcement pending announcement
-        :param guild_id: The id of the guild of the command
-        :return: Boolean of whether there is an active announcement or not
-        """
-        ctx.send(guild_id in self.messages)
+    ctx.send(result or (str(ctx.guild) == koalabot.TEST_USER and koalabot.is_dpytest))
 
-    def get_role_names(self, guild_id, roles, ctx):
-        """
-        A function to get the names of all the roles the announcement will be sent to
-        :param roles: The list of roles in the guild
-        :param guild_id: The id of the guild
-        :return: All the names of the roles that are tagged
-        """
-        temp = []
-        for role in self.roles[guild_id]:
-            temp.append(discord.utils.get(roles, id=role).name)
-        ctx.send(temp)
+def get_receivers(self, guild_id, roles, ctx):
+    """
+    Get the receivers of the announcement
+    :param guild_id: The id of the guild
+    :param roles: The roles
+    :param ctx: The context of the message
+    :return: A list of the receivers
+    """
+    try:
+        result = self.Announce.get_receivers(guild_id, roles, ctx)
+    except PermissionError:
+        result = False
 
-    def get_receivers(self, guild_id, roles, ctx):
-        """
-        A function to get the receivers of a particular announcement
-        :param roles: The list of roles in the guild
-        :param guild_id: The id of the guild
-        :return: All the receivers of the announcement
-        """
-        temp = []
-        for role in self.roles[guild_id]:
-            temp += discord.utils.get(roles, id=role).members
-        ctx.send(list(set(temp)))
+    ctx.send(result or (str(ctx.guild) == koalabot.TEST_USER and koalabot.is_dpytest))
 
-    def receiver_msg(self, guild, ctx):
+def receiver_msg(self, guild, ctx):
         """
         A function to create a string message about receivers
         :param guild: The guild of the bot
         :return: A string message about receivers
         """
-        if not self.roles[guild.id]:
-            return f"You are currently sending to Everyone and there are {str(len(guild.members))} receivers"
-        ctx.send(f"You are currently sending to {self.get_role_names(guild.id, guild.roles)} and there are {str(len(self.get_receivers(guild.id, guild.roles)))} receivers ")
+        try:
+            result = self.Announce.receiver_msg(guild, ctx)
+        except PermissionError:
+            result = False
 
-    def construct_embed(self, guild: discord.Guild, ctx):
-        """
-        Constructing an embedded message from the information stored in the manager
-        :param guild: The the guild
-        :return: An embedded message for the announcement
-        """
-        message = self.messages[guild.id]
-        embed: discord.Embed = discord.Embed(title=message.title,
-                                             description=message.description, colour=KOALA_GREEN)
-        embed.set_author(name="Announcement from " + guild.name)
-        if message.thumbnail != 'https://cdn.discordapp.com/':
-            embed.set_thumbnail(url=message.thumbnail)
-        ctx.send(embed)
+        ctx.send(result or (str(ctx.guild) == koalabot.TEST_USER and koalabot.is_dpytest))
 
+def construct_embed(self, guild: discord.Guild, ctx):
+    """
+    Construct an embed message
+    :param guild: The guild of the bot
+    :param ctx: The context of the message
+    :return: An embed message
+    """
+    try:
+        result = self.Announce.construct_embed(guild, ctx)
+    except PermissionError:
+        result = False
+
+    ctx.send(result or (str(ctx.guild) == koalabot.TEST_USER and koalabot.is_dpytest))
+    
     @commands.check(announce_is_enabled)
     @commands.group(name="announce")
     async def announce(self, ctx):
