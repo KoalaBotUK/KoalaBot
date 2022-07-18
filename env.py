a14b275114a95aee5066f990cf92658b1ec6ab48
Charles <107768424+DatGuyChucky@users.noreply.github.com>
2022-07-18 16:35:42 +0800

    koalabot.is_Owner overhaul

---
+++ env.py
@@ -0,0 +1,22 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.environ['DISCORD_TOKEN']
+BOT_OWNER = os.environ.get('BOT_OWNER')
+
+API_PORT = os.environ.get("API_PORT", 8080)
+
+ADMINS = os.getenv('ADMINS')
+
+DB_KEY = os.environ.get('SQLITE_KEY', "2DD29CA851E7B56E4697B0E1F08507293D761A05CE4D1B628663F411A8086D99")
+ENCRYPTED_DB = (not os.name == 'nt') and eval(os.environ.get('ENCRYPTED', "True"))
+
+CONFIG_PATH = os.environ.get("CONFIG_PATH")
+if not CONFIG_PATH:
+    CONFIG_PATH = "/config"
+    if os.name == 'nt':
+        CONFIG_PATH = '.'+CONFIG_PATH
+
+LOGGING_FILE = eval(os.environ.get("LOGGING_FILE", "True"))
--- koalabot.py
+++ koalabot.py
@@ -31,7 +31,7 @@ from discord.ext import commands
 from koala.db import extension_enabled
 from koala.utils import error_embed
 from koala.log import logger
-from koala.env import BOT_TOKEN, BOT_OWNER, API_PORT
+from koala.env import BOT_TOKEN, BOT_OWNER, API_PORT, ADMINS
 
 # Constants
 COMMAND_PREFIX = "k!"
@@ -58,7 +58,8 @@ is_dpytest = False
 
 def is_owner(ctx):
     """
-    A command used to check if the user of a command is the owner, or the testing bot
+    A command used to check if the user of a command is the owner, or the testing bot.
+    The command also allows Senior Devs of KoalaBot to use owner only commands (as given by Admin role in the dev portal)
     e.g. @commands.check(koalabot.is_owner)
     :param ctx: The context of the message
     :return: True if owner or test, False otherwise
@@ -66,9 +67,15 @@ def is_owner(ctx):
     if is_dm_channel(ctx):
         return False
     elif BOT_OWNER is not None:
-        return ctx.author.id == int(BOT_OWNER) or is_dpytest
+        if ctx.author.id == int(BOT_OWNER):
+            return True
+        else:
+            return ctx.message.author.name in ADMINS or is_dpytest
     else:
-        return bot.is_owner(ctx.author) or is_dpytest
+        if ctx.message.author.name in ADMINS:
+            return True
+        else:
+            return bot.is_owner(ctx.author) or is_dpytest
 
 
 def is_admin(ctx):
