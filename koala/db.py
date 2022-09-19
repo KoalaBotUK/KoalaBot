f7264bf4b445e940abf007d54e3af70fe69fe2f9
JayDwee <drapj002@gmail.com>
2022-09-19 19:46:36 +0100

    fix: add pool_recycle to remove stale pools

--- koala/db.py
+++ koala/db.py
@@ -24,7 +24,7 @@ from koala.log import logger
 # Constants
 
 # Variables
-engine = create_engine(DB_URL, future=True, pool_size=20)
+engine = create_engine(DB_URL, future=True, pool_size=20, pool_recycle=3600)
 Session = sessionmaker(future=True)
 Session.configure(bind=engine)
 
