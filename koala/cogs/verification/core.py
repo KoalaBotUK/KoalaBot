8cc6d2f1d668c7aa55e03361058d07e89c5aa1dd
Joe <musicdudejoe@googlemail.com>
2022-10-17 20:13:43 +0100

    Refactored sessions

--- koala/cogs/verification/core.py
+++ koala/cogs/verification/core.py
@@ -7,11 +7,12 @@ from email.message import EmailMessage
 # Libs
 import discord
 from discord.ext import commands
+import sqlalchemy.orm
 from sqlalchemy import select, delete, and_, text
 
 # Own modules
 import koalabot
-from koala.db import session_manager, insert_extension
+from koala.db import assign_session, session_manager, insert_extension
 from .env import GMAIL_EMAIL, GMAIL_PASSWORD
 from .log import logger
 from .models import VerifiedEmails, NonVerifiedEmails, Roles, ToReVerify
@@ -43,7 +44,8 @@ def send_email(email, token):
     email_server.send_message(msg)
     email_server.quit()
 
-def enable_verification(guild_id, guild_roles, suffix=None, role=None):
+@assign_session
+def enable_verification(guild_id, guild_roles, session: sqlalchemy.orm.Session, suffix=None, role=None):
     """
     Set up a role and email pair for KoalaBot to verify users with
     :param guild_id: guild id of current guild
@@ -52,32 +54,32 @@ def enable_verification(guild_id, guild_roles, suffix=None, role=None):
     :param role: the role to give users with that email verified (e.g. @students)
     :return:
     """
-    with session_manager() as session:
-        if not role or not suffix:
-            raise InvalidArgumentError(f"Please provide the correct arguments\n(`{koalabot.COMMAND_PREFIX}enable_verification <domain> <@role>`")
+    if not role or not suffix:
+        raise InvalidArgumentError(f"Please provide the correct arguments\n(`{koalabot.COMMAND_PREFIX}enable_verification <domain> <@role>`")
 
-        try:
-            role_id = int(role[3:-1])
-        except ValueError:
-            raise InvalidArgumentError("Please give a role by @mentioning it")
-        except TypeError:
-            raise InvalidArgumentError("Please give a role by @mentioning it")
+    try:
+        role_id = int(role[3:-1])
+    except ValueError:
+        raise InvalidArgumentError("Please give a role by @mentioning it")
+    except TypeError:
+        raise InvalidArgumentError("Please give a role by @mentioning it")
 
-        role_valid = discord.utils.get(guild_roles, id=role_id)
-        if not role_valid:
-            raise InvalidArgumentError("Please mention a role in this guild")
+    role_valid = discord.utils.get(guild_roles, id=role_id)
+    if not role_valid:
+        raise InvalidArgumentError("Please mention a role in this guild")
 
-        exists = session.execute(select(Roles)
-                                    .filter_by(s_id=guild_id, r_id=role_id, email_suffix=suffix)).all()
-        if exists:
-            raise VerifyError("Verification is already enabled for that role")
+    exists = session.execute(select(Roles)
+                                .filter_by(s_id=guild_id, r_id=role_id, email_suffix=suffix)).all()
+    if exists:
+        raise VerifyError("Verification is already enabled for that role")
 
-        session.add(Roles(s_id=guild_id, r_id=role_id, email_suffix=suffix))
-        session.commit()
+    session.add(Roles(s_id=guild_id, r_id=role_id, email_suffix=suffix))
+    session.commit()
 
-        return role_valid
+    return role_valid
 
-def disable_verification(guild_id, suffix=None, role=None):
+@assign_session
+def disable_verification(guild_id, session: sqlalchemy.orm.Session, suffix=None, role=None):
     """
     Disable an existing verification listener
     :param guild_id: guild id of current guild
@@ -85,96 +87,96 @@ def disable_verification(guild_id, suffix=None, role=None):
     :param role: the role paired with the email (e.g. @students)
     :return:
     """
-    with session_manager() as session:
-        if not role or not suffix:
-            raise InvalidArgumentError(
-                f"Please provide the correct arguments\n(`{koalabot.COMMAND_PREFIX}enable_verification <domain> <@role>`")
+    if not role or not suffix:
+        raise InvalidArgumentError(
+            f"Please provide the correct arguments\n(`{koalabot.COMMAND_PREFIX}enable_verification <domain> <@role>`")
 
-        try:
-            role_id = int(role[3:-1])
-        except ValueError:
-            raise InvalidArgumentError("Please give a role by @mentioning it")
-        except TypeError:
-            raise InvalidArgumentError("Please give a role by @mentioning it")
+    try:
+        role_id = int(role[3:-1])
+    except ValueError:
+        raise InvalidArgumentError("Please give a role by @mentioning it")
+    except TypeError:
+        raise InvalidArgumentError("Please give a role by @mentioning it")
 
-        session.execute(delete(Roles).filter_by(s_id=guild_id, r_id=role_id, email_suffix=suffix))
-        session.commit()
+    session.execute(delete(Roles).filter_by(s_id=guild_id, r_id=role_id, email_suffix=suffix))
+    session.commit()
 
-def verify(user_id, email):
+@assign_session
+def verify(user_id, email, session: sqlalchemy.orm.Session):
     """
     Send to KoalaBot in dms to verify an email with our system
     :param user_id: id of user to be verified
     :param email: the email you want to verify
     :return:
     """
-    with session_manager() as session:
-        already_verified = session.execute(select(VerifiedEmails).filter_by(email=email)).all()
+    already_verified = session.execute(select(VerifiedEmails).filter_by(email=email)).all()
 
-        in_blacklist = session.execute(select(ToReVerify).filter_by(u_id=user_id)).all()
+    in_blacklist = session.execute(select(ToReVerify).filter_by(u_id=user_id)).all()
 
-        if already_verified and not in_blacklist:
-            raise VerifyError("That email is already verified")
+    if already_verified and not in_blacklist:
+        raise VerifyError("That email is already verified")
 
-        verification_code = ''.join(random.choice(string.ascii_letters) for _ in range(8))
-        session.add(NonVerifiedEmails(u_id=user_id, email=email, token=verification_code))
-        session.commit()
+    verification_code = ''.join(random.choice(string.ascii_letters) for _ in range(8))
+    session.add(NonVerifiedEmails(u_id=user_id, email=email, token=verification_code))
+    session.commit()
 
-        send_email(email, verification_code)
+    send_email(email, verification_code)
 
-def un_verify(user_id, email):
+@assign_session
+def un_verify(user_id, email, session: sqlalchemy.orm.Session):
     """
     Send to KoalaBot in dms to un-verify an email with our system
     :param user_id: id of user to be un-verified
     :param email: the email you want to un-verify
     :return:
     """
-    with session_manager() as session:
-        entry = session.execute(select(VerifiedEmails).filter_by(u_id=user_id, email=email)).all()
+    entry = session.execute(select(VerifiedEmails).filter_by(u_id=user_id, email=email)).all()
 
-        if not entry:
-            raise VerifyError("You have not verified that email")
+    if not entry:
+        raise VerifyError("You have not verified that email")
 
-        session.execute(delete(VerifiedEmails).filter_by(u_id=user_id, email=email))
-        session.commit()
+    session.execute(delete(VerifiedEmails).filter_by(u_id=user_id, email=email))
+    session.commit()
 
-def confirm(user_id, token):
-    with session_manager() as session:
-        entry = session.execute(select(NonVerifiedEmails).filter_by(token=token)).scalar()
+@assign_session
+def confirm(user_id, token, session: sqlalchemy.orm.Session):
+    entry = session.execute(select(NonVerifiedEmails).filter_by(token=token)).scalar()
 
-        if not entry:
-            raise InvalidArgumentError("That is not a valid token")
+    if not entry:
+        raise InvalidArgumentError("That is not a valid token")
 
-        already_verified = session.execute(select(VerifiedEmails)
-                                            .filter_by(u_id=user_id, email=entry.email)).all()
+    already_verified = session.execute(select(VerifiedEmails)
+                                        .filter_by(u_id=user_id, email=entry.email)).all()
 
-        if not already_verified:
-            session.add(VerifiedEmails(u_id=ctx.author.id, email=entry.email))
+    if not already_verified:
+        session.add(VerifiedEmails(u_id=user_id, email=entry.email))
 
-        session.execute(delete(NonVerifiedEmails).filter_by(token=token))
+    session.execute(delete(NonVerifiedEmails).filter_by(token=token))
 
-        potential_roles = session.execute(select(Roles.r_id)
-                                            .where(text(":email like ('%' || email_suffix)")),
-                                            {"email": entry.email}).all()
-        if potential_roles:
-            for role_id in potential_roles:
-                session.execute(delete(ToReVerify).filter_by(r_id=role_id[0], u_id=user_id))
+    potential_roles = session.execute(select(Roles.r_id)
+                                        .where(text(":email like ('%' || email_suffix)")),
+                                        {"email": entry.email}).all()
+    if potential_roles:
+        for role_id in potential_roles:
+            session.execute(delete(ToReVerify).filter_by(r_id=role_id[0], u_id=user_id))
 
-        session.commit()
+    session.commit()
 
     return entry
 
-def get_emails(user_id: int):
+@assign_session
+def get_emails(user_id: int, session: sqlalchemy.orm.Session):
     """
     See the emails a user is verified with
     :param user_id: the id of the user who's emails you want to find
     :return: string of emails user is verified with
     """
-    with session_manager() as session:
-        results = session.execute(select(VerifiedEmails.email).filter_by(u_id=user_id)).all()
+    results = session.execute(select(VerifiedEmails.email).filter_by(u_id=user_id)).all()
 
-        return '\n'.join([x[0] for x in results])
+    return '\n'.join([x[0] for x in results])
 
-async def re_verify(role, guild_id, guild_roles, guild_members):
+@assign_session
+async def re_verify(role, guild_id, guild_roles, guild_members, session: sqlalchemy.orm.Session):
     """
     Removes a role from all users who have it and marks them as needing to re-verify before giving it back
     :param role: the role to be removed and re-verified (e.g. @students)
@@ -183,25 +185,24 @@ async def re_verify(role, guild_id, guild_roles, guild_members):
     :param guild_members: all members in the current guild
     :return:
     """
-    with session_manager() as session:
-        try:
-            role_id = int(role[3:-1])
-        except ValueError:
-            raise InvalidArgumentError("Please give a role by @mentioning it")
-        except TypeError:
-            raise InvalidArgumentError("Please give a role by @mentioning it")
+    try:
+        role_id = int(role[3:-1])
+    except ValueError:
+        raise InvalidArgumentError("Please give a role by @mentioning it")
+    except TypeError:
+        raise InvalidArgumentError("Please give a role by @mentioning it")
 
-        exists = session.execute(select(Roles).filter_by(s_id=guild_id, r_id=role_id)).all()
+    exists = session.execute(select(Roles).filter_by(s_id=guild_id, r_id=role_id)).all()
 
-        if not exists:
-            raise VerifyError("Verification is not enabled for that role")
-        role = discord.utils.get(guild_roles, id=role_id)
-        for member in guild_members:
-            if role in member.roles:
-                await member.remove_roles(role)
-                session.add(ToReVerify(u_id=member.id, r_id=role.id))
+    if not exists:
+        raise VerifyError("Verification is not enabled for that role")
+    role = discord.utils.get(guild_roles, id=role_id)
+    for member in guild_members:
+        if role in member.roles:
+            await member.remove_roles(role)
+            session.add(ToReVerify(u_id=member.id, r_id=role.id))
 
-        session.commit()
+    session.commit()
 
 class InvalidArgumentError(Exception):
     pass
