2a5b64401266e83dc324444757b3638400c1fb36
Otto Hooper <votton.hooper@gmail.com>
2022-10-17 17:30:48 +0100

    Try add api as a import to init

--- koala/cogs/announce/__init__.py
+++ koala/cogs/announce/__init__.py
@@ -1,3 +1,10 @@
-from . import utils, db, log, models
-from .cog import Announce, setup
+from koala.cogs.announce import api
+
+from . import db, log, models, utils
 from .announce_message import AnnounceMessage
+from .cog import Announce, setup
+
+
+def setup(bot):
+    cog.setup(bot)
+    api.setup(bot)
