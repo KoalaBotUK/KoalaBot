3aee5020056196cb9512d4e0ae62153ec3fb8516
Charles <charleschengck@gmail.com>
2023-04-04 19:36:23 +0100

    Merging extra stuff from master

--- koala/env.py
+++ koala/env.py
@@ -4,11 +4,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.environ['DISCORD_TOKEN']
-BOT_OWNER = os.environ.get('BOT_OWNER')
+BOT_OWNER_ENV = os.environ.get('BOT_OWNER')
+BOT_OWNER_STR = BOT_OWNER_ENV.split(',')
+BOT_OWNER = [int(item) for item in BOT_OWNER_STR]
 
 API_PORT = os.environ.get("API_PORT", 8080)
-
-ADMINS = os.getenv('ADMINS')
 
 DB_KEY = os.environ.get('SQLITE_KEY', "2DD29CA851E7B56E4697B0E1F08507293D761A05CE4D1B628663F411A8086D99")
 ENCRYPTED_DB = (not os.name == 'nt') and eval(os.environ.get('ENCRYPTED', "True"))
