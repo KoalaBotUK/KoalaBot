a637193d6462096b0dd334c66c4fef3d91ac723f
abluey <zoe1615@hotmail.com>
2023-05-03 00:12:49 +0100

    test: core options, end time, stubs

--- tests/cogs/voting/test_core.py
+++ tests/cogs/voting/test_core.py
@@ -100,7 +100,8 @@ async def test_set_chair_no_dms(bot: commands.Bot, cog):
     chair: discord.Member = guild.members[1]
     core.start_vote(bot, cog.vote_manager, "Test Vote", author, guild)
 
-    # dpytest.back.start_private_message
+    # dpytest.back.start_private_message?
+    # pytest.raises is NOT the way to go here. the Forbidden is excepted, not thrown.
     with mock.patch('discord.channel.DMChannel', mock.Mock(side_effect=Exception('discord.Forbidden'))):
         with pytest.raises(discord.Forbidden, match="Chair not set as requested user is not accepting direct messages."):
             await core.set_chair(cog.vote_manager, author, chair)
@@ -116,8 +117,7 @@ async def test_set_no_chair(bot: commands.Bot, cog):
 
 
 # make_voice_channel doesn't exist even though it's in their documentation
-@pytest.mark.asyncio
-async def test_set_channel(bot: commands.Bot, cog):
+def test_set_channel(bot: commands.Bot, cog):
     guild: discord.Guild = dpytest.get_config().guilds[0]
     author: discord.Member = guild.members[0]
     channel = dpytest.back.make_voice_channel("Voice Channel", guild)
@@ -127,11 +127,140 @@ async def test_set_channel(bot: commands.Bot, cog):
     assert core.set_channel(cog.vote_manager, author, channel) == f"Set target channel to {channel.name}"
 
 
-@pytest.mark.asyncio
-async def test_set_no_channel(bot: commands.Bot, cog):
+def test_set_no_channel(bot: commands.Bot, cog):
     guild: discord.Guild = dpytest.get_config().guilds[0]
     author: discord.Member = guild.members[0]
 
     core.start_vote(bot, cog.vote_manager, "Test Vote", author, guild)
 
     assert core.set_channel(cog.vote_manager, author) == "Removed channel restriction on vote"
+
+
+def test_add_option(bot: commands.Bot, cog):
+    guild: discord.Guild = dpytest.get_config().guilds[0]
+    author: discord.Member = guild.members[0]
+
+    core.start_vote(bot, cog.vote_manager, "Test Vote", author, guild)
+
+    assert core.add_option(cog.vote_manager, author, "Option 1+Option description") == "Option Option 1 with description Option description added to vote"
+
+def test_add_option_wrong_formatting(bot: commands.Bot, cog):
+    guild: discord.Guild = dpytest.get_config().guilds[0]
+    author: discord.Member = guild.members[0]
+
+    core.start_vote(bot, cog.vote_manager, "Test Vote", author, guild)
+
+    assert core.add_option(cog.vote_manager, author, "Option 1") == "Example usage: k!vote addOption option title+option description"
+
+
+def test_add_option_too_many(bot: commands.Bot, cog):
+    guild: discord.Guild = dpytest.get_config().guilds[0]
+    author: discord.Member = guild.members[0]
+
+    core.start_vote(bot, cog.vote_manager, "Test Vote", author, guild)
+    
+    x = 0
+    while (x < 11):
+        core.add_option(cog.vote_manager, author, "more+options")
+        x += 1
+
+    assert core.add_option(cog.vote_manager, author, "more options+please?") == "Vote has maximum number of options already (10)"
+
+
+def test_add_option_too_long(bot: commands.Bot, cog):
+    guild: discord.Guild = dpytest.get_config().guilds[0]
+    author: discord.Member = guild.members[0]
+    core.start_vote(bot, cog.vote_manager, "Test Vote", author, guild)
+
+    test_option = "i am trying to write a lot of words here. needs to be over fifteen thousand words to be exact. but i'm separating it so it does not all get clustered into the same paragraph and become a word soup+i was wrong, it is actually fifteen hundred words. whoever actually reads this will get a little entertainment i hope. is there a better way to test this? probably."
+    x = 0
+    while (x < 5):
+        core.add_option(cog.vote_manager, author, test_option)
+        x += 1
+
+    assert core.add_option(cog.vote_manager, author, test_option) == "Option string is too long. The total length of all the vote options cannot be over 1500 characters."
+
+
+def test_remove_option(bot: commands.Bot, cog):
+    guild: discord.Guild = dpytest.get_config().guilds[0]
+    author: discord.Member = guild.members[0]
+    core.start_vote(bot, cog.vote_manager, "Test Vote", author, guild)
+
+    core.add_option(cog.vote_manager, author, "test+option")
+
+    assert core.remove_option(cog.vote_manager, author, 0) == "Option number 0 removed"
+
+
+def test_remove_option_bad(bot: commands.Bot, cog):
+    guild: discord.Guild = dpytest.get_config().guilds[0]
+    author: discord.Member = guild.members[0]
+    core.start_vote(bot, cog.vote_manager, "Test Vote", author, guild)
+
+    assert core.remove_option(cog.vote_manager, author, 0) == "Option number 0 not found"
+
+
+def test_set_end_time(bot: commands.Bot, cog):
+    guild: discord.Guild = dpytest.get_config().guilds[0]
+    author: discord.Member = guild.members[0]
+    core.start_vote(bot, cog.vote_manager, "Test Vote", author, guild)
+
+    assert core.set_end_time(cog.vote_manager, author, "2222-12-30 13:30") == "Vote set to end at 2222-12-30 13:30:00 UTC"
+
+
+def test_set_impossible_end_time(bot: commands.Bot, cog):
+    guild: discord.Guild = dpytest.get_config().guilds[0]
+    author: discord.Member = guild.members[0]
+    core.start_vote(bot, cog.vote_manager, "Test Vote", author, guild)
+
+    assert core.set_end_time(cog.vote_manager, author, "2020-01-15 12:50") == "You can't set a vote to end in the past"
+
+
+def test_preview(bot: commands.Bot, cog):
+    # guild: discord.Guild = dpytest.get_config().guilds[0]
+    # author: discord.Member = guild.members[0]
+    # core.start_vote(bot, cog.vote_manager, "Test Vote", author, guild)
+
+    pass
+
+
+def test_cancel_vote(bot: commands.Bot, cog):
+    # guild: discord.Guild = dpytest.get_config().guilds[0]
+    # author: discord.Member = guild.members[0]
+    # core.start_vote(bot, cog.vote_manager, "Test Vote", author, guild)
+
+    pass
+
+
+def test_current_votes(bot: commands.Bot, cog):
+    # guild: discord.Guild = dpytest.get_config().guilds[0]
+    # author: discord.Member = guild.members[0]
+    # core.start_vote(bot, cog.vote_manager, "Test Vote", author, guild)
+
+    pass
+
+
+@pytest.mark.asyncio
+async def test_close(bot: commands.Bot, cog):
+    # guild: discord.Guild = dpytest.get_config().guilds[0]
+    # author: discord.Member = guild.members[0]
+    # core.start_vote(bot, cog.vote_manager, "Test Vote", author, guild)
+
+    pass
+
+
+@pytest.mark.asyncio
+async def test_send_vote(bot: commands.Bot, cog):
+    # guild: discord.Guild = dpytest.get_config().guilds[0]
+    # author: discord.Member = guild.members[0]
+    # core.start_vote(bot, cog.vote_manager, "Test Vote", author, guild)
+
+    pass
+
+
+@pytest.mark.asyncio
+async def test_get_results(bot: commands.Bot, cog):
+    # guild: discord.Guild = dpytest.get_config().guilds[0]
+    # author: discord.Member = guild.members[0]
+    # core.start_vote(bot, cog.vote_manager, "Test Vote", author, guild)
+
+    pass
